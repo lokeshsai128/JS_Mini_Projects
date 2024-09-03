8fdec95bd1bd68f0a8ad6209274b5acf1708f0ad
lokeshsai128 <lokeshsai128@gmail.com>
2024-09-03 17:05:25 +0530

    added second_project

---
+++ 02_Auto_text_animation_effect/index.js
@@ -0,0 +1,25 @@
+const containerEl = document.querySelector(".container");
+
+let careers = ["Youtuber", "Web Developer", "Freelancer","Instructor"]
+
+let careerIndex = 0;
+
+let characterIndex = 0;
+
+textUpdate();
+
+function textUpdate() {
+    characterIndex++
+    containerEl.innerHTML = `<h1>I am ${careers[careerIndex].slice(0,1) === "I" ? "an" : "a"} ${careers[careerIndex].slice(0,characterIndex)}</h1>`;
+    if(characterIndex === careers[careerIndex].length){
+        careerIndex++
+        characterIndex = 0
+    }
+    if(careerIndex === careers.length){
+        careerIndex = 0
+    }
+    setTimeout(textUpdate,250)
+}
+
+
+
