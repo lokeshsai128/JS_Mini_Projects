96377d8ddfeedec2d82303e66014a420bcf9b112
lokeshsai128 <lokeshsai128@gmail.com>
2024-09-04 12:29:13 +0530

    04_Button_ripple_effect:

--- 04_Button_ripple_effect/index.js
+++ 04_Button_ripple_effect/index.js
@@ -1,9 +1,9 @@
-//not yet working
-const btnEl = document.querySelector(".btn")
+const btnEl = document.querySelector(".btn");
 
-btnEl.addEventListener("mouseover",() => {
-    const x = event.pageX - btnEl.offsetLeft;
-    const y = event.pageY - btnEl.offsetTop;
-    btnEl.style.setProperty("--xPos", x+"px");
-    btnE1.style.setProperty("-yPos", y + "px");  
-})
+btnEl.addEventListener("mouseover", (event) => {
+  const x = event.pageX - btnEl.offsetLeft;
+  const y = event.pageY - btnEl.offsetTop;
+
+  btnEl.style.setProperty("--xPos", x + "px");
+  btnEl.style.setProperty("--yPos", y + "px");
+});
