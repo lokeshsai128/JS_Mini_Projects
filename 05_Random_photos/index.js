2444d87b8e0a1c1107ca4f8250b15db1c9e7d86d
lokeshsai128 <lokeshsai128@gmail.com>
2024-09-04 13:11:49 +0530

    05_Random_photos/index:

--- 05_Random_photos/index.js
+++ 05_Random_photos/index.js
@@ -3,5 +3,13 @@ const imagecontainerEl = document.querySelector(".image-container");
 const btnEl = document.querySelector(".btn");
 
 btnEl.addEventListener("click",() => {
+    let imageNum = 10;
+    addnewImages()
     console.log("clicked");
 })
+
+function addnewImages(){
+    const newimgEl = document.createElement("img");
+    newimgEl.src = `https://picsum.photos/300?random=${Math.floor(Math.random()*2000)}`;
+    imagecontainerEl.appendChild(newimgEl);
+}
