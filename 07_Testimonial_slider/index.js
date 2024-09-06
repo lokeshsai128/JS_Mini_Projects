25acdaa5a46115367492fa3564b4e7576c8bb040
lokeshsai128 <lokeshsai128@gmail.com>
2024-09-06 10:02:23 +0530

    07_Testimonial_slider/index:

--- 07_Testimonial_slider/index.js
+++ 07_Testimonial_slider/index.js
@@ -1,4 +1,4 @@
-const testimonial = [
+const testimonials = [
     {
         name : "Julia Rodriguez",
         photoUrl : "https://images.unsplash.com/photo-1598550880863-4e8aa3d0edb4?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fHww",
@@ -15,3 +15,21 @@ const testimonial = [
         text : "I am extremely satisfied with Apple's innovative phone. The sleek design, advanced features, and user-friendly interface make it a must-have device for anyone looking for a top-of-the-line mobile experience."
     }
 ]
+
+const imgEl = document.querySelector("img");
+const textEl = document.querySelector(".text");
+const usernameEl = document.querySelector(".username");
+
+
+let index = 0;
+
+function updateTestimonial(){
+    const {name,photoUrl,text} = testimonials[index];
+    imgEl.src=photoUrl;
+    textEl.innerText = text;
+    usernameEl.innerText = name;
+    index++
+    setTimeout(() => {
+        updateTestimonial();
+    }, 2000);
+}
